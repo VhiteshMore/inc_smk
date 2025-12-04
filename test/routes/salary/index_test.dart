598bfbec7d2091e7d84319d7388fd91c9b50932d
vhitesh <vhiteshm17@gmail.com>
2025-12-05 01:24:36 +0530

    -- test: add passing test cases for netSalaryCalculation

--- test/routes/salary/index_test.dart
+++ test/routes/salary/index_test.dart
@@ -4,17 +4,31 @@ import 'package:incubyte_smk/utils/validators.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('CalculateSalary if India 10% deduction', () async {
+  test('CalculateSalary if India then 10% deduction', () async {
     expect(
       _calculateNetSalary(country: 'india', gross: 100),
       equals((10.0, 90.0)),
     );
   });
 
-  test('CalculateSalary if US 12% deduction', () async {
+  test('CalculateSalary if US other 12% deduction', () async {
     expect(
       _calculateNetSalary(country: '', gross: 100),
       equals((12.0, 88.0)),
+    );
+  });
+
+  test('CalculateSalary if other then 0% deduction', () async {
+    expect(
+      _calculateNetSalary(country: 'norway', gross: 100),
+      equals((100.0, 0.0)),
+    );
+  });
+
+  test('CalculateSalary if (country is empty) or (gross <0)', () async {
+    expect(
+      _calculateNetSalary(country: 'norway', gross: -1),
+      equals((-1.0, -1.0)),
     );
   });
 }
@@ -24,12 +38,16 @@ void main() {
   required double gross,
 }) {
 
-  double deductions = 0;
+  if (country.isEmpty || gross < 0) {
+    return (-1.0, -1.0);
+  }
+
+  double deductions = gross;
 
   if (country == 'india') {
-    deductions = gross * 0.15;
-  } else {
-    deductions = gross;
+    deductions = gross * 0.10;
+  } else if (country == 'united states') {
+    deductions = gross * 0.12;
   }
   return (deductions, gross - deductions);
 }
