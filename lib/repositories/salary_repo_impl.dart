18e1f01ee98b39a86a0e1c6d17ab7174010dd7b0
vhitesh <vhiteshm17@gmail.com>
2025-12-05 02:05:18 +0530

    -- added try catch for db operation in SalaryRepoImpl

--- lib/repositories/salary_repo_impl.dart
+++ lib/repositories/salary_repo_impl.dart
@@ -12,22 +12,26 @@ class SalaryRepoImpl extends SalaryRepo {
   @override
   Future<SalaryDto> calculateSalary(int employee_id) async {
 
-    final employee = await (_appDb.select(_appDb.employee)..where((e) => e.id.equals(employee_id))).getSingleOrNull();
-    if (employee == null) {
-      return SalaryDto.empty();
-    }
+    try {
+      final employee = await (_appDb.select(_appDb.employee)..where((e) => e.id.equals(employee_id))).getSingleOrNull();
+      if (employee == null) {
+        return SalaryDto.empty();
+      }
 
-    final (deductions, netSalary) = SalaryService.calculateNetSalary(
-      country: employee.country,
-      gross: employee.salary,
-    );
-
-    return SalaryDto(
-        employee_id: employee.id,
-        gross_salary: employee.salary,
+      final (deductions, netSalary) = SalaryService.calculateNetSalary(
         country: employee.country,
-        deductions: deductions,
-        net_salary: netSalary);
+        gross: employee.salary,
+      );
+
+      return SalaryDto(
+          employee_id: employee.id,
+          gross_salary: employee.salary,
+          country: employee.country,
+          deductions: deductions,
+          net_salary: netSalary);
+    } catch (_) {
+      rethrow;
+    }
   }
 
 
