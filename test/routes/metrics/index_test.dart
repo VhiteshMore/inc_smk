827925e8a19be0e0452d2d318ba579c1a81bdb77
vhitesh <vhiteshm17@gmail.com>
2025-12-05 03:05:45 +0530

    -- test: add failing test cases for metrics which fails due to reduce operator failure for empty list

---
+++ test/routes/metrics/index_test.dart
@@ -0,0 +1,54 @@
+import 'package:incubyte_smk/dtos/employee_dto.dart';
+import 'package:test/test.dart';
+
+void main() {
+
+  List<EmployeeDto> employeeList = [
+    // EmployeeDto(id: 1, fullName: 'test', jobTitle: 'react_dev', country: 'india', salary: 90),
+    // EmployeeDto(id: 2, fullName: 'test1', jobTitle: 'react_dev', country: 'india', salary: 100),
+    // EmployeeDto(id: 3, fullName: 'test3', jobTitle: 'flutter_dev', country: 'india', salary: 110),
+    // EmployeeDto(id: 4, fullName: 'test4', jobTitle: 'flutter_dev', country: 'us', salary: 90),
+    // EmployeeDto(id: 5, fullName: 'test5', jobTitle: 'flutter_dev', country: 'us', salary: 110),
+  ];
+
+  test('getCountryMetric if country is india', () {
+
+    final salaryList = employeeList.where((elm) => elm.country == 'india').map((e) => e.salary).toList();
+
+    final minSalary = _getMinSalary(salaryList);
+    final maxSalary = _getMaxSalary(salaryList);
+    final avgSalary = _getAvgSalary(salaryList);
+
+    expect(
+      (minSalary, maxSalary, avgSalary),
+      equals((90.0, 110.0, 100.0)),
+    );
+  });
+
+  test('getJobTitleMetric if jobTitle is flutter_dev', () async {
+
+    final salaryList = employeeList.where((elm) => elm.jobTitle == 'flutter_dev').map((e) => e.salary).toList();
+
+    final avgSalary = _getAvgSalary(salaryList);
+
+    expect(avgSalary, 95.0);
+  });
+}
+
+double _getMinSalary(List<double> list) {
+  double min = -1;
+  min = list.reduce((a, b) => a < b ? a : b);
+  return min;
+}
+
+double _getMaxSalary(List<double> list) {
+  double max = -1;
+  max = list.reduce((a, b) => a > b ? a : b);
+  return max;
+}
+
+double _getAvgSalary(List<double> list) {
+  double avg = -1;
+  avg = list.reduce((a, b) => a + b) / list.length;
+  return avg;
+}
